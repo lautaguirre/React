69e8f6d44260a94a39f63ba158dd0764fc61e2d2
lautaguirre <32684961+lautaguirre@users.noreply.github.com>
2017-12-28 15:28:08 -0300

    toggle component ; conditional rendering ; nested component in clock

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,31 +2,96 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Welcome component
 class Welcome extends Component{
   render(){
-    return <h3>Hola, {this.props.name}</h3>
+    return <h3>Hola {this.props.name}!</h3>
   }
 }
 
-const elem=<Welcome name='Joan' />;
+//JSX var
+const elem=<Welcome name='Juan' />;
 
-class Clock extends Component{
+//Date comp
+class Formatteddate extends Component{
+  render(){
+    return(
+      <h4>{this.props.date.toString()}</h4>
+    );
+  }
+}
+
+//Conditional rendering
+class UserGreeting extends Component {
+  render(){
+    return <h1>Welcome back!</h1>;
+  }
+}
+
+class GuestGreeting extends Component {
+  render(){
+    return <h1>Please sign up.</h1>;
+  }
+}
+
+class Greeting extends Component{
+  render(){
+    if(this.props.isLoggedIn){
+      return <UserGreeting />;
+    }else{
+      return <GuestGreeting />;
+    }
+  }
+}
+
+//Toggle comp
+class Toggle extends Component{
   constructor(props){
     super(props);
-    this.state = {date: new Date()};
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  handleClick(){
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render(){
+    return(
+      //To pass a parameter to the event handler {this.deleteRow.bind(this, param)}
+      <button onClick = {this.handleClick}>
+        {this.state.isToggleOn ? 'ON':'OFF'}
+      </button>
+    );
+  }
+
+}
+
+//Clock component
+class Clock extends Component{
+  constructor(props){ //Init component elements
+    super(props);
+    this.state = {date: new Date()}; //Init date state
+  }
+
+  //When component rendered
   componentDidMount(){
     this.timerID = setInterval(
-      () => this.tick(),
+      () => this.tick(), //Arrow function to define parameters
       1000
     );
   }
 
+  //When component is removed
   componentWillUnmount(){
     clearInterval(this.timerID);
   }
 
+  //Update date state
   tick(){
     this.setState({
       date: new Date()
@@ -35,7 +100,10 @@ class Clock extends Component{
 
   render(){
     return(
-      <h1>{this.state.date.toLocaleTimeString()}</h1>
+      <div>
+        <h1>{this.state.date.toLocaleTimeString()}</h1>
+        <Formatteddate date={this.state.date} />
+      </div>
     );
   }
 }
@@ -51,9 +119,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          {elem}
-        </p>
+        {elem} 
+        <Toggle />
+        <Greeting isLoggedIn = {true}/>
       </div>
     );
   }
