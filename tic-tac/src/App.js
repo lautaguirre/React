99a87676f761eddbf929e65c9092c115409584a6
lautaguirre <32684961+lautaguirre@users.noreply.github.com>
2018-01-26 11:53:39 -0300

    Added tic-tac practice

---
+++ tic-tac/src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+
+class Square extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      value: null
+    }
+  }
+  render() {
+    return (
+      <button onClick={() => this.setState({value: 'X'})} className="square">
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p>
+          <Game />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
